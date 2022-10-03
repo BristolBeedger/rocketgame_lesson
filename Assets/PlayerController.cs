cfa84521f6e294bcbf1b12c5f80f57064f3ea2c7
BristolBeedger <bristolbeedger@outlook.com>
2022-10-03 14:45:25 -0400

    Updated movement framework

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -13,6 +13,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        if(Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Pressed SPACE");
+        }
+    }
+    void ProcessRotation() {
+        if(Input.GetKey(KeyCode.D)) {
+            Debug.Log("Rotate Clockwise");
+        }
+        else if(Input.GetKey(KeyCode.A)) {
+            Debug.Log("Rotate CounterClockwise");
+        }
     }
 }
