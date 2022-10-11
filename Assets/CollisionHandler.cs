6ac48293fbb425d7d6fad86a3f48dca0c4362511
BristolBeedger <bristolbeedger@outlook.com>
2022-10-11 14:42:28 -0400

    Creating Reload Logic

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) {
-        
+    void OnCollisionEnter(Collision other) {
+        switch (other.gameObject.tag)
+        {
+            case "Finish":
+                Debug.Log("You have reached the Finish!"); 
+                break;
+            case "Friendly":
+                Debug.Log("This is a friendly object!");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel Added");
+                break;
+            default:
+                ReloadLevel(SceneManager.GetActiveScene());
+                break;
+        }
+    }
+
+    void ReloadLevel(Scene activeLevel) {
+        SceneManager.LoadScene(activeLevel.name);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,11 +9,13 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody rb;
     Transform tr;
+    AudioSource thrustAudio;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tr = GetComponent<Transform>();
+        thrustAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,16 +28,17 @@ public class PlayerController : MonoBehaviour
     void ProcessThrust() {
         if(Input.GetKey(KeyCode.Space)) {
             rb.AddRelativeForce(Vector3.up*thrustMod*Time.deltaTime);
+            if(!thrustAudio.isPlaying)
+                thrustAudio.Play();
         }
+        else
+            thrustAudio.Stop();
     }
     void ProcessRotation() {
         if(Input.GetKey(KeyCode.D))
-        {
             ApplyRotation(Vector3.back);
-        }
-        else if(Input.GetKey(KeyCode.A)) {
+        else if(Input.GetKey(KeyCode.A))
             ApplyRotation(Vector3.forward);
-        }
     }
 
     void ApplyRotation(Vector3 direction)
